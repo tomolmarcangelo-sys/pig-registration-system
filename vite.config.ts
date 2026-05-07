7b85cc3abc114963f349e9c703c6d16f112e0391
Marc Angelo Tomol <tomolmarcangelo@gmail.com>
2026-05-07 23:05:31 +0800

    fix: move build plugins to dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,27 @@
 import path from "path";
 import { fileURLToPath } from "url";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  plugins: [
+    react(), 
+    tailwindcss(), 
+    viteSingleFile()
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    target: "esnext",
+    assetsInlineLimit: 100000000, // Important for single file builds
+    chunkSizeWarningLimit: 100000000,
   },
 });
